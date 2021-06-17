8b2f05d6d4655d6c8129399d0fde740de0689c1c
Sajedeh Gooklani <gookalani@mail.com>
2021-06-17 18:47:33 +0300

    Added responsive device icons
    - widescreen
    - laptop
    - tablet
    - mobile

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-export { default as check } from './library/check';
+export { default as laptop } from './library/laptop';
+export { default as mobile } from './library/mobile';
+export { default as tablet } from './library/tablet';
+export { default as widescreen } from './library/widescreen';
