3e2b0ced3f9acc37bf65d7d47f43c23e5615210d
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-06-23 17:29:03 +0300

    Ran lint

--- src/library/tablet.js
+++ src/library/tablet.js
@@ -1,27 +1,27 @@
 export default {
-    id: 'tablet',
-    paths: [
-        {
-            d: 'M6.5 5h11v14h-11V5z',
-            attrs: {
-                fill: '#0094FF',
-            },
-        },
-        {
-            d: 'M15.125 5L6.5 17.075V19h.75l10-14h-2.125zM10.2 19h6.05l1.25-1.75V8.57c-1.6 2.345-5.35 7.84-7.3 10.43z',
-            attrs: {
-                fill: '#FFF',
-                opacity: '.25',
-            },
-        },
-        {
-            d:
-                'M6.5 4a1 1 0 00-1 1v14a1 1 0 001 1h11a1 1 0 001-1V5a1 1 0 00-1-1h-11zM7 5a.5.5 0 00-.5.5v13a.5.5 0 00.5.5h10a.5.5 0 00.5-.5v-13A.5.5 0 0017 5H7z',
-            attrs: {
-                fillRule: 'evenodd',
-                clipRule: 'evenodd',
-                fill: '#1E1E1E',
-            },
-        },
-    ],
-}
+	id: 'tablet',
+	paths: [
+		{
+			d: 'M6.5 5h11v14h-11V5z',
+			attrs: {
+				fill: '#0094FF',
+			},
+		},
+		{
+			d: 'M15.125 5L6.5 17.075V19h.75l10-14h-2.125zM10.2 19h6.05l1.25-1.75V8.57c-1.6 2.345-5.35 7.84-7.3 10.43z',
+			attrs: {
+				fill: '#FFF',
+				opacity: '.25',
+			},
+		},
+		{
+			d:
+				'M6.5 4a1 1 0 00-1 1v14a1 1 0 001 1h11a1 1 0 001-1V5a1 1 0 00-1-1h-11zM7 5a.5.5 0 00-.5.5v13a.5.5 0 00.5.5h10a.5.5 0 00.5-.5v-13A.5.5 0 0017 5H7z',
+			attrs: {
+				fillRule: 'evenodd',
+				clipRule: 'evenodd',
+				fill: '#1E1E1E',
+			},
+		},
+	],
+};
--- src/library/widescreen.js
+++ src/library/widescreen.js
@@ -1,27 +1,26 @@
 export default {
-    id: 'widescreen',
-    paths: [
-        {
-            d: 'M5 6h14v9H5V6z',
-            attrs: {
-                fill: '#0094FF',
-            },
-        },
-        {
-            d: 'M15.977 6L5 13.762V15h.955l12.727-9h-2.705zM9.71 15h7.7L19 13.875v-5.58c-2.036 1.508-6.81 5.04-9.29 6.705z',
-            attrs: {
-                fill: '#FFF',
-                opacity: '.25',
-            },
-        },
-        {
-            d:
-                'M5 5a1 1 0 00-1 1v9a1 1 0 001 1h5.5v2h-2a.5.5 0 000 1h7a.5.5 0 000-1h-2v-2H19a1 1 0 001-1V6a1 1 0 00-1-1H5zm8 10h6V6H5v9h8zm-1.5 1h1v2h-1v-2z',
-            attrs: {
-                fillRule: 'evenodd',
-                clipRule: 'evenodd',
-                fill: '#1E1E1E',
-            },
-        },
-    ],
-}
+	id: 'widescreen',
+	paths: [
+		{
+			d: 'M5 6h14v9H5V6z',
+			attrs: {
+				fill: '#0094FF',
+			},
+		},
+		{
+			d: 'M15.977 6L5 13.762V15h.955l12.727-9h-2.705zM9.71 15h7.7L19 13.875v-5.58c-2.036 1.508-6.81 5.04-9.29 6.705z',
+			attrs: {
+				fill: '#FFF',
+				opacity: '.25',
+			},
+		},
+		{
+			d: 'M5 5a1 1 0 00-1 1v9a1 1 0 001 1h5.5v2h-2a.5.5 0 000 1h7a.5.5 0 000-1h-2v-2H19a1 1 0 001-1V6a1 1 0 00-1-1H5zm8 10h6V6H5v9h8zm-1.5 1h1v2h-1v-2z',
+			attrs: {
+				fillRule: 'evenodd',
+				clipRule: 'evenodd',
+				fill: '#1E1E1E',
+			},
+		},
+	],
+};
