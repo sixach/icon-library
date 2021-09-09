141aca333c9c324886be17c2a849c5405491c26a
Sajedeh Gooklani <gookalani@mail.com>
2021-09-09 23:09:21 +0300

    Added Forms icon

---
+++ src/library/forms.js
@@ -0,0 +1,31 @@
+export default {
+	id: 'forms',
+	paths: [
+		{
+			d: 'M5 5h14v4H5V5zm0 6h14v3H5v-3zm2 5a1 1 0 00-1 1v2a1 1 0 001 1h10a1 1 0 001-1v-2a1 1 0 00-1-1H7z',
+			attrs: {
+				fillRule: 'evenodd',
+				clipRule: 'evenodd',
+				fill: '#0094FF',
+			},
+		},
+		{
+			d: 'M19 5H5v1h14V5zm0 6H5v1h14v-1z',
+			attrs: {
+				fillRule: 'evenodd',
+				clipRule: 'evenodd',
+				fill: '#FFF',
+				opacity: '.25',
+			},
+		},
+		{
+			d:
+				'M5 4a1 1 0 00-1 1v3a1 1 0 001 1h14a1 1 0 001-1V5a1 1 0 00-1-1H5zm0 1h14v3H5V5zm0 5a1 1 0 00-1 1v3a1 1 0 001 1h14a1 1 0 001-1v-3a1 1 0 00-1-1H5zm0 4v-3h14v3H5z',
+			attrs: {
+				fillRule: 'evenodd',
+				clipRule: 'evenodd',
+				fill: '#1E1E1E',
+			},
+		},
+	],
+};
