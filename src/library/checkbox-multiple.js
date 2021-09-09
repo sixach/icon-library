b42598b1628a8ce0bbcba70cd70681139e54afb3
Sajedeh Gooklani <gookalani@mail.com>
2021-09-09 23:06:36 +0300

    Added Checkbox multiple icon

---
+++ src/library/checkbox-multiple.js
@@ -0,0 +1,31 @@
+export default {
+	id: 'checkbox-multiple',
+	paths: [
+		{
+			d: 'M10 5H5v5h5V5zm9 0h-5v5h5V5zM5 14h5v5H5v-5zm14 0h-5v5h5v-5z',
+			attrs: {
+				fillRule: 'evenodd',
+				clipRule: 'evenodd',
+				fill: '#0094FF',
+			},
+		},
+		{
+			d: 'M6 5H5v5h1V5zm9 0h-1v5h1V5zM5 14h1v5H5v-5zm10 0h-1v5h1v-5z',
+			attrs: {
+				fillRule: 'evenodd',
+				clipRule: 'evenodd',
+				fill: '#FFF',
+				opacity: '.25',
+			},
+		},
+		{
+			d:
+				'M4 5a1 1 0 011-1h5a1 1 0 011 1v5a1 1 0 01-1 1H5a1 1 0 01-1-1V5zm1 0v5h5V5H5zm8 0a1 1 0 011-1h5a1 1 0 011 1v5a1 1 0 01-1 1h-5a1 1 0 01-1-1V5zm1 0v5h5V5h-5zm-9 8a1 1 0 00-1 1v5a1 1 0 001 1h5a1 1 0 001-1v-5a1 1 0 00-1-1H5zm0 6v-5h5v5H5zm8-5a1 1 0 011-1h5a1 1 0 011 1v5a1 1 0 01-1 1h-5a1 1 0 01-1-1v-5zm1 0v5h5v-5h-5zM9.354 6.146a.5.5 0 010 .708l-2 2a.5.5 0 01-.631.062l-1-.667a.5.5 0 01.554-.832l.66.44 1.71-1.71a.5.5 0 01.707 0zm9 0a.5.5 0 010 .708l-2 2a.5.5 0 01-.631.062l-1-.667a.5.5 0 01.554-.832l.66.44 1.71-1.71a.5.5 0 01.707 0zm-9 9.708a.5.5 0 00-.708-.708l-1.71 1.71-.659-.439a.5.5 0 10-.554.832l1 .667a.5.5 0 00.63-.062l2-2zm9-.708a.5.5 0 010 .708l-2 2a.5.5 0 01-.631.062l-1-.667a.5.5 0 01.554-.832l.66.44 1.71-1.71a.5.5 0 01.707 0z',
+			attrs: {
+				fillRule: 'evenodd',
+				clipRule: 'evenodd',
+				fill: '#1E1E1E',
+			},
+		},
+	],
+};
