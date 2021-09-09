a5d77fb5b96822b545315a8c5260ae19bcd0d6f2
Sajedeh Gooklani <gookalani@mail.com>
2021-09-09 23:04:14 +0300

    Added Accordion icon

---
+++ src/library/accordion.js
@@ -0,0 +1,29 @@
+export default {
+	id: 'accordion',
+	paths: [
+		{
+			d: 'M19 5H5v1h14V5zm0 13H5v1h14v-1zM5 7h14v1H5V7zm12 2H7v8h10V9z',
+			attrs: {
+				fillRule: 'evenodd',
+				clipRule: 'evenodd',
+				fill: '#0094FF',
+			},
+		},
+		{
+			d: 'M7 9h10v2H7z',
+			attrs: {
+				fill: '#FFF',
+				opacity: '.25',
+			},
+		},
+		{
+			d:
+				'M4 5a1 1 0 011-1h14a1 1 0 011 1v3a1 1 0 01-1 1h-1v8h1a1 1 0 011 1v1a1 1 0 01-1 1H5a1 1 0 01-1-1v-1a1 1 0 011-1h1V9H5a1 1 0 01-1-1V5zm3 12h10V9H7v8zM6 5H5v1h14V5H6zM5 7h14v1H5V7zm0 11h14v1H5v-1z',
+			attrs: {
+				fillRule: 'evenodd',
+				clipRule: 'evenodd',
+				fill: '#1E1E1E',
+			},
+		},
+	],
+};
