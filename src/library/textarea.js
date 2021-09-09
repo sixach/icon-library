9e12d264d2ebad40c29008834ce0a66a10dc66c9
Sajedeh Gooklani <gookalani@mail.com>
2021-09-09 23:11:51 +0300

    Added Textarea icon

---
+++ src/library/textarea.js
@@ -0,0 +1,27 @@
+export default {
+	id: 'texarea',
+	paths: [
+		{
+			d: 'M5 7h14v10H5V7z',
+			attrs: {
+				fill: '#0094FF',
+			},
+		},
+		{
+			d: 'M5 8h14V7H5v1z',
+			attrs: {
+				fill: '#FFF',
+				opacity: '.25',
+			},
+		},
+		{
+			d:
+				'M4 7a1 1 0 011-1h14a1 1 0 011 1v10a1 1 0 01-1 1H5a1 1 0 01-1-1V7zm1 0v10h14V7H5z',
+			attrs: {
+				fillRule: 'evenodd',
+				clipRule: 'evenodd',
+				fill: '#1E1E1E',
+			},
+		},
+	],
+};
